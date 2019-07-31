29dada60063647344261516c5306fbf6d5f5944d
Uanderson Lima Santos <uandersonlimacs@gmail.com>
2019-07-30 21:28:47 -0300

    Commit 2

--- GITTEST/WebApplication1/Controllers/HomeController.cs
+++ GITTEST/WebApplication1/Controllers/HomeController.cs
@@ -19,6 +19,10 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public IActionResult Commit()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
